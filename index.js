8bf972899fcb2c80fa8ad3d8d0930eb7192cc7ef
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-09-10 10:45:33 +0200

    added <h1> and hover states

--- index.js
+++ index.js
@@ -11,12 +11,11 @@ if (leadsFromLocalStorage) {
   render(myLeads);
 }
 
-tabBtn.addEventListener("click", function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    myLeads.push(tabs[0].url);
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    render(myLeads);
-  });
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 });
 
 deleteBtn.addEventListener("dblclick", function () {
@@ -25,22 +24,24 @@ deleteBtn.addEventListener("dblclick", function () {
   render(myLeads);
 });
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  render(myLeads);
-  function render(leads) {
-    let listItems = "";
-    for (let i = 0; i < leads.length; i++) {
-      listItems += `
-                <li>
-                    <a target='_blank' href='${leads[i]}'>
-                        ${leads[i]}
-                    </a>
-                </li>
-            `;
-    }
-    ulEl.innerHTML = listItems;
-  }
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
 });
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+              <li>
+                  <a target='_blank' href='${leads[i]}'>
+                      ${leads[i]}
+                  </a>
+              </li>
+          `;
+  }
+  ulEl.innerHTML = listItems;
+}
